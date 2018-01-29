3a99c500bede090fb6b158ba446c811b59ad938e
Raphael Lutz <raphael.lutz@etu.unige.ch>
2018-01-29 14:18:54 +0100

    Add files via upload

--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -15,8 +15,34 @@ public class MarkingGraph {
 public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
-        // Write here the implementation of the marking graph generation.
-        return nil
-    }
+        // noeud initial
+        let noeud = MarkingGraph(marking : marking)
+        // liste des noeuds déjà vu (vide à l'initialisation)
+        var nodeSeen = [MarkingGraph]()
+        // liste des noeuds à traiter (1 élément à l'initialisation)
+        var nodeToDo = [noeud]
 
+        // si la liste nodeToDo contient au moins 1 élément
+        while let currentNode = nodeToDo.popLast() {
+
+          nodeSeen.append(currentNode)
+
+          for trans in self.transitions {
+            if let marq = trans.fire(from: currentNode.marking) {
+              // si le noeud a déjà été visité
+              if nodeSeen.contains(where: {$0.marking == marq}) {
+                currentNode.successors[trans] = nodeSeen.first(where: {$0.marking == marq})}
+              // si le noeud doit être visité
+              else if nodeToDo.contains(where: {$0.marking == marq}) {
+                currentNode.successors[trans] = nodeToDo.first(where: {$0.marking == marq})}
+              // sinon, il s'agit d'un nouveau noeud
+              else {
+                let newNode = MarkingGraph(marking: marq)
+                nodeToDo.append(newNode)
+                currentNode.successors[trans] = newNode}
+            }
+          }
+        }
+        return noeud
+    }
 }
